6dc6895a47405adc6e9068219c9648b5485864c2
sarahmaheen <sarahmaheen02@gmail.com>
2024-07-05 13:06:47 +0530

    added backend url again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,41 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   server: {
+//     host:'localhost',
+//     port:'5173',
+//     proxy: {
+//       '/api':'https://lms-mern-ten.vercel.app',
+//     },
+//   },
+//   plugins: [react()],
+// });
+
+
+
+
+
+
+
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    host:'localhost',
-    port:'5173',
+    host: 'localhost',
+    port: '5173',
     proxy: {
-      '/api':'https://lms-mern-ten.vercel.app',
+      '/api': {
+        target: 'https://lms-mern-ten.vercel.app',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // Optional: Remove '/api' prefix
+      },
     },
   },
   plugins: [react()],
 });
+
