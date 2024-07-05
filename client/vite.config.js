ded88e18d4cdb5c72c4766332ebfa31029369a61
sarahmaheen <sarahmaheen02@gmail.com>
2024-07-05 13:23:41 +0530

    vercel removed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,41 +1,13 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   server: {
-//     host:'localhost',
-//     port:'5173',
-//     proxy: {
-//       '/api':'https://lms-mern-ten.vercel.app',
-//     },
-//   },
-//   plugins: [react()],
-// });
-
-
-
-
-
-
-
-
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    host: 'localhost',
-    port: '5173',
+    host:'localhost',
+    port:'5173',
     proxy: {
-      '/api': {
-        target: 'https://lms-mern-ten.vercel.app',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '') // Optional: Remove '/api' prefix
-      },
+      '/api':'http://localhost:5010',
     },
   },
   plugins: [react()],
 });
-
